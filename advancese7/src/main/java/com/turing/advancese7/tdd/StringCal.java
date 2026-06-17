fbf15859af185f74991800d7a244bfb86c42c623
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2026-06-17 21:36:41 +0630

    add String Calculator

--- advancese7/src/main/java/com/turing/advancese7/tdd/StringCal.java
+++ advancese7/src/main/java/com/turing/advancese7/tdd/StringCal.java
@@ -1,17 +1,162 @@
 package com.turing.advancese7.tdd;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class StringCal {
 
-	public static double sum(String input) {
+	static final int INITIAL_STATE =0;
+	static final int INPUT_READING_STATE =1;
+	
+	List<Character> seperators = new ArrayList<>();
+	public StringCal()
+	{
+		seperators.add(',');
+		seperators.add('\n');
+	}
+	boolean isSeperator(char ch)
+	{
+		for(char sep : this.seperators)
+		{
+			if(ch==sep)
+			{
+				
+				return true;
+			}
+		}
+		return false;
+	}
+	boolean isValidInput(char ch)
+	{
+		return Character.isDigit(ch) || ch =='.';
+	}
+	List<String> parseToken(String input)
+	{
+		List<String> numberList = new ArrayList<>();
+		String numberToken = "";
+		
+		int state = INITIAL_STATE;//Input reading
+		System.out.println("Input ==> "+input);
+		
+		for(int i=0;i<input.length();i++)
+		{
+			char ch = input.charAt(i);
+			System.out.println("Current ch "+ch);
+			switch(state)
+			{
+				case INITIAL_STATE://input accept
+					
+					if(this.isValidInput(ch))
+					{
+						System.out.println("Valid input "+ch);
+						numberToken+= ch;
+					}
+					else if(this.isSeperator(ch))
+					{
+						System.out.println("seperator case "+ch);
+						numberList.add(numberToken);
+						state = INPUT_READING_STATE;
+						numberToken="";
+					}
+					else
+					{
+						throw new RuntimeException(this.seperators.get(0) +" expected but found "+ch+" at position "+i);
+					}
+				break;	
+				case INPUT_READING_STATE://seperator
+					if(isSeperator(ch))
+					{
+						//Problem
+						System.out.println("Error state position "+i);
+						throw new RuntimeException("Number expected but "+ch+" found at position "+i);
+					}
+					else if(this.isValidInput(ch))
+					{
+						state = INITIAL_STATE;
+						numberToken= ""+ch;
+					}
+					else
+					{
+						throw new RuntimeException(this.seperators.get(0) +" expected but found "+ch+" at position "+i);
+					}
+			}
+		}
+		System.out.println("State "+state+ " token "+numberToken);
+		if(!numberToken.isEmpty())
+		{
+			numberList.add(numberToken);
+		}
+		else if(state==INPUT_READING_STATE && numberToken.isEmpty())
+		{
+			System.out.println("This case");
+			throw new RuntimeException("Number expected but EOF found.");
+		}
+		return numberList;
+	}
+	public String sum(String input) {
 		
 		if(input == null || input.isEmpty())
 		{
-			return 0;
+			return 0+"";
+		}
+		else if(input.startsWith("//"))
+		{
+			return sumWithCustomSeperator(input);
 		}
 		else
 		{
-			return Double.parseDouble(input);
+			return sumListOfNumber(input);
+			
 		}
+	}
+	private String sumWithCustomSeperator(String input) {
+		int indexOfNewLine = input.indexOf('\n');
+				
+		String sep = input.substring(2,indexOfNewLine);
+		
+		System.out.println("Sep==> "+sep);
+		seperators = new ArrayList<>();
+		seperators.add(';');
+		
+		String numList=input.substring(indexOfNewLine+1).replace(sep, ";");
+		System.out.println("Remaining input ==> "+numList);
+		return sumListOfNumber(numList);
+	}
+	private String sumListOfNumber(String input) {
+		try
+		{
+			double total = sumNumber(input);
+			return removeDotZeroSuffix(total);
+		}
+		catch(Exception e)
+		{
+			return e.getMessage();
+		}
+	}
+	private String removeDotZeroSuffix(double total) {
+		String output = String.valueOf(total);
+		if(output.endsWith(".0"))
+		{
+			return output.substring(0,output.indexOf("."));
+		}
+		else
+		{
+			return output;
+		}
+	}
+	private double sumNumber(String input) {
+		List<String> tokens = parseToken(input);
+		double total = 0;
+		
+		for(String token : tokens)
+		{
+			System.out.println("Token "+token);
+			total += Double.parseDouble(token);
+		}
+		return total;
 	}
 
 }
--- advancese7/src/test/java/com/turing/advancese7/tdd/StringCalculatorTest.java
+++ advancese7/src/test/java/com/turing/advancese7/tdd/StringCalculatorTest.java
@@ -8,20 +8,40 @@ import org.junit.jupiter.api.Test;
 //https://kata-log.rocks/string-calculator-kata
 public class StringCalculatorTest {
 
+	StringCal cal = new StringCal();
 	@Test
 	void testNullCase()
 	{
-		assertEquals(0,StringCal.sum(null));
-		assertEquals(0,StringCal.sum(""));
+		assertEquals("0",cal.sum(null));
+		assertEquals("0",cal.sum(""));
 	}
 	@Test
 	void testSingleNumber()
 	{
-		assertEquals(1,StringCal.sum("1"));
-		assertEquals(2,StringCal.sum("2"));
-		assertEquals(23,StringCal.sum("23"));
-		assertEquals(2.3,StringCal.sum("2.3"));
-		assertEquals(.3,StringCal.sum(".3"));
+		assertEquals("1",cal.sum("1"));
+		assertEquals("2",cal.sum("2"));
+		assertEquals("23",cal.sum("23"));
+		assertEquals("2.3",cal.sum("2.3"));
+		assertEquals("0.3",cal.sum(".3"));
 		
+	}
+	
+	@Test
+	void testMultipleNumber()
+	{
+		assertEquals("6",cal.sum("1\n2,3"));
+		
+		assertEquals("Number expected but \n"
+				+ " found at position 6",cal.sum("175.2,\n35"));
+				
+		assertEquals("Number expected but EOF found.",cal.sum("1,3,"));
+	}
+	@Test
+	void testCustomSeperator()
+	{
+		assertEquals("3",cal.sum("//;\n1;2"));
+		assertEquals("6",cal.sum("//|\n1|2|3"));
+		assertEquals("5",cal.sum("//sep\n2sep3"));
+		assertEquals("; expected but found , at position 3",cal.sum("//|\n1|2,3"));
 	}
 }
